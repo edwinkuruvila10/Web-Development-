0ffed2b81ee561b51fa7415e7de6c21ecdcafc1e
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-03-31 14:39:58 +0530

    Functions of JS

---
+++ HTML/tut53.js
@@ -0,0 +1,16 @@
+console.log("This is tutorial 53");
+
+function greet(name, greenText="Greeting from javascript"){
+    console.log(greenText + " " + name);
+    console.log(name + " is a good boy");
+}
+
+let name = "edwin";
+let name1 = "rohan";
+let name2 = "das";
+let name3 = "rajjj";
+let greenText = "Good morning";
+greet(name, greenText);
+greet(name1, greenText);
+greet(name2, greenText);
+greet(name3);
